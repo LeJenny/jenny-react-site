d5695a12d87b707956f7c03abd5609cabf471551
Jenny Le <jenny.k.le@sjsu.edu>
2023-08-24 17:31:14 -0700

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Specify the paths to all of your template files
+    "./public/index.html", // Also include the path to your public HTML file, if you use any Tailwind classes there
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
